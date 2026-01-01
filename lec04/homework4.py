be9fe4166e2bdb5c48d7b3c68a05475cdf51002c
M24W7455-Kumarasinghe Mudiyanselage Chamara Madhusanka Kumarasinghe <122626793+chamara17@users.noreply.github.com>
2026-01-01 15:33:01 +0900

    homework

--- lec04/homework4.py
+++ lec04/homework4.py
@@ -1,4 +1,3 @@
-
 def next_birthday(date, birthdays):
     '''
     Find the next birthday after the given date.
@@ -12,7 +11,25 @@ def next_birthday(date, birthdays):
     birthday - the next day, after given date, on which somebody has a birthday
     list_of_names - list of all people with birthdays on that date
     '''
-    birthday = (1,1)
-    list_of_names = []
-    return birthday, list_of_names
+    month, day = date
+    
+    # We loop indefinitely until we find a match
+    while True:
+        # 1. Advance to the next day
+        day += 1
+        
+        # 2. Handle end of month (Simplified logic from lecture: 
+        # just assume max 31 days. Invalid dates won't be in the dict anyway.)
+        if day >= 32:
+            day = 1
+            month += 1
+            
+        # 3. Handle end of year
+        if month >= 13:
+            month = 1
+            
+        # 4. Check if this new date exists in the dictionary
+        if (month, day) in birthdays:
+            # Found it! Return the date and the list of names
+            return (month, day), birthdays[(month, day)]
     
